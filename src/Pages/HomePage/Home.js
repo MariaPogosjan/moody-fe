ad900116b4978a2b42c9bae4fa1021d4c1056a36
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-06-03 15:54:30 +0200

    Created basic routing and redux

---
+++ src/Pages/HomePage/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { Container } from 'styled-components/Containers.js'
+
+const Home = () => {
+  return (
+    <Container>
+      <p>Home</p>
+    </Container>
+    
+  )
+}
+
+export default Home
